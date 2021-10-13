70d92d7de5fc93a699c032dbb5337aa07625f54c
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-13 09:49:25 -0700

    Adds display_product method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,11 @@ class ProductsController < ApplicationController
     content = Product.all
     render json: content
   end
+
+  def display_product
+    render :json (message: "Which product would you like to see today? (Enter a Product ID Number)")
+    product = gets.chomp.to_i
+    content = Product.find_by(id:product)
+    render json: content
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/display_all" => "products#display_all"
+  get "displey_product" => "products#display_product"
 end
