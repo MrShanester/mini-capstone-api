44fb26fe865c541ae2698de79d84d8cbd0abc0dd
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-15 11:08:16 -0700

    Adds test requests to requests.http and adds delete and update actions to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,27 @@ class ProductsController < ApplicationController
     output = Product.find_by id: input
     render json: output.as_json
   end
+
+  def create
+    product = Product.new(name: params["name"], price: params["price"], image_url: params["image_url"], description: params["description"])
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    input = params["id"].to_i
+    product = Product.find_by id: input
+    product.destroy
+  end
+
+  def update
+    input = params["id"]
+    product = Product.find_by id: input
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+    product.save
+    render json: product.as_json
+  end
 end
