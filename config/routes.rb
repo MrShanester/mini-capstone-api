dcdbf4c1acb57f7e78af8661c821bf903a9f7f39
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-15 10:23:04 -0700

    Adds conventional routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
