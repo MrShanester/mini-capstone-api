1aeac0d94a3fb3e6475f18d32e0c777540d6495e
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-18 12:15:25 -0700

    Adds friendly created_at

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,4 +16,9 @@ class Product < ApplicationRecord
     total = tax + price
     return total
   end
+
+  def friendly_created_at
+    friend = created_at.strftime("%A, %d %b %Y %l:%M %p")
+    return friend
+  end
 end
