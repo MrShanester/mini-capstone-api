5147c969de41db92b54d7df169f863f220adedfe
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-20 10:02:55 -0700

    Adds validation to proper file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,4 @@
 class ProductsController < ApplicationController
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { maximum: 500 }
-  validates :price, presence: true, numericality: { only_integer: true }
-  validates :image_url, presence: true
 
   def index
     output = Product.all
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :image_url, presence: true
+  
   def is_discounted?
     price < 10
   end
