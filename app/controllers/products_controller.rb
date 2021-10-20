9c717a45ecf40ebaa34085d092b00b70b092c98c
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-20 10:55:02 -0700

    Adds error messages to create and update actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,11 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(name: params["name"], price: params["price"], image_url: params["image_url"], description: params["description"])
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else 
+      render json: {error: product.errors.full_messages}
+    end
   end
 
   def destroy
@@ -31,7 +34,10 @@ class ProductsController < ApplicationController
     product.price = params["price"] || product.price
     product.image_url = params["image_url"] || product.image_url
     product.description = params["description"] || product.description
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else 
+      render json: {error: product.errors.full_messages}
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
-  validates :price, presence: true, numericality: { only_integer: true }, greater_than: 0
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0}
   validates :image_url, presence: true
 
   def is_discounted?
