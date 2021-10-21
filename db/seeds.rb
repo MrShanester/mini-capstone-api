883b8c4cea2f07a9e3aa66d1695ba1c4c7142e6e
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-21 10:42:44 -0700

    Adds supplier model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 # Product.create(name: "Trampoline", price: 450, image_url: "https://m.media-amazon.com/images/I/91Erb6gHeIL._AC_SL1500_.jpg", description: "Bouncy")
 # Product.create(name: "Garage", price: 45000, image_url: "https://www.cnet.com/a/img/resize/bf4b014ad7c37fc4d5f803293539cbd40c9a19d5/hub/2020/02/13/5a3d16da-470d-4663-8530-45d0a0cabb8a/hta-bunker-front-2.jpg?auto=webp&fit=crop&height=1293&width=2300", description: "Roomy and secure.")
 # Product.create(name: "Statue", price: 14000, image_url: "https://i.guim.co.uk/img/media/bab7c53fc9db0764fb29a2991021ef7f68d42464/0_762_3427_2057/master/3427.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=5d6feaf53c98fb5c0425c7e1f858bb1b", description: "Made in your image.")
+# Supplier.create(name: "IBM", email: "IBM@example.com", phone_number: "555-4545")
+# Supplier.create(name: "Whole Foods", email: "Wholefoods@example.com", phone_number: "535-3535")
+# Supplier.create(name: "Amazon Warehouse", email: "amazing@example.com", phone_number: "666-4545")
