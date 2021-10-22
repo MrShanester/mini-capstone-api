949fae3cbcae535cfa5c9f5d2dc35de224d394ee
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-22 10:38:30 -0700

    Adds image models to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,17 @@
 # Supplier.create(name: "IBM", email: "IBM@example.com", phone_number: "555-4545")
 # Supplier.create(name: "Whole Foods", email: "Wholefoods@example.com", phone_number: "535-3535")
 # Supplier.create(name: "Amazon Warehouse", email: "amazing@example.com", phone_number: "666-4545")
+# Image.create(url: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Shure_mikrofon_55S.jpg", product_id: 11)
+# Image.create(url: "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/figs-2056729.jpg?webp=true&quality=90&resize=408%2C370", product_id: 5)
+# Image.create(url: "https://m.media-amazon.com/images/I/91Erb6gHeIL._AC_SL1500_.jpg", product_id: 1)
+# Image.create(url: "https://cdn.vox-cdn.com/thumbor/oVBg90OZHOM5R2aIkj3u1rEHuz0=/0x0:1200x800/920x613/filters:focal(504x304:696x496):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/59013955/spacesuit.0.jpg", product_id: 7)
+# Image.create(url: "https://i.guim.co.uk/img/media/bab7c53fc9db0764fb29a2991021ef7f68d42464/0_762_3427_2057/master/3427.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=5d6feaf53c98fb5c0425c7e1f858bb1b", product_id: 3)
+# Image.create(url: "https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8=
+#   ", product_id: 13)
+# Image.create(url: "https://www.atomicheritage.org/sites/default/files/Becquerelite-Uraninite-38634.jpg", product_id: 9)
+# Image.create(url: "https://m.media-amazon.com/images/I/61ydxRILX6L._AC_SL1500_.jpg", product_id: 15)
+# Image.create(url: "https://www.upr.org/sites/upr/files/styles/medium/public/201712/material.default.dirt_.png", product_id: 16)
+# Image.create(url: "https://www.cnet.com/a/img/resize/bf4b014ad7c37fc4d5f803293539cbd40c9a19d5/hub/2020/02/13/5a3d16da-470d-4663-8530-45d0a0cabb8a/hta-bunker-front-2.jpg?auto=webp&fit=crop&height=1293&width=2300", product_id: 2)
+# Image.create(url: "https://www.worldhistory.org/img/r/p/500x600/14382.jpg?v=1627781402", product_id: 8)
+# Image.create(url: "https://image.shutterstock.com/image-photo/wooden-stick-isolated-on-white-260nw-234176800.jpg", product_id: 10)
+
