69c4ef7875d64a723f0d58a92609b14cabe67c24
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-25 11:56:34 -0700

    Adds authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,51 +1,72 @@
 class OrdersController < ApplicationController
   def index
-    output = Order.all 
-    render json: output.as_json
+    order = current_user.orders.all
+    if current_user
+      render json: order
+    else
+      render json: {message: "User not logged in."}
+    end
   end
 
   def show
-    input = params["id"]
-    order = Order.find_by id: input
-    render json: order.as_json
+    if current_user
+      order = current_user.orders.find_by(id: params[:id]) 
+      render json: order
+    else
+      render json: {message: "Unauthorized"}, status: 401
+    end
   end
 
   def create
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      subtotal: Product.find_by(id: params["product_id"]).price * params["quantity"],
-      tax: Product.find_by(id: params["product_id"]).tax * params["quantity"],
-      total: Product.find_by(id: params["product_id"]).total * params["quantity"]
-    )
-    if order.save 
-      render json: order.as_json
-    else 
-      render json: {error: order.errors.full_messages}, status: 406
+    if current_user
+      product = Product.find_by(id: params["product_id"])
+      quantity = params["quantity"]
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        subtotal: product.price * quantity,
+        tax: product.tax * quantity,
+        total: product.total * quantity
+      )
+      if order.save 
+        render json: order
+      else 
+        render json: {error: order.errors.full_messages}, status: 406
+      end
+    else
+      render json: {message: "Unauthorized"}, status: 401
     end
   end
 
   def update
-    input = params["id"]
-    order = Order.find_by id: input
-    order.user_id = params["user_id"] || order.user_id
-    order.product_id = params["product_id"] || order.product_id
-    order.quantity = params["quantity"] || order.quantity
-    order.subtotal = params["subtotal"] || order.subtotal
-    order.tax = params["tax"] || order.tax
-    order.total = params["total"] || order.total
-    if order.save 
-      render json: order.as_json
-    else 
-      render json: {error: order.errors.full_messages}, status: 406
+    if current_user
+      input = params["id"]
+      order = Order.find_by id: input
+      order.user_id = params["user_id"] || order.user_id
+      order.product_id = params["product_id"] || order.product_id
+      order.quantity = params["quantity"] || order.quantity
+      order.subtotal = params["subtotal"] || order.subtotal
+      order.tax = params["tax"] || order.tax
+      order.total = params["total"] || order.total
+      if order.save 
+        render json: order
+      else 
+        render json: {error: order.errors.full_messages}, status: 406
+      end
+    else
+      render json: {message: "Unauthorized"}, status: 401
     end
   end
 
   def destroy
-    input = params["id"]
-    order = Order.find_by id: input
-    order.destroy
-    render json: {message: "Order Destroyed."}
+    if current_user
+      input = params["id"]
+      order = Order.find_by id: input
+      order.destroy
+      render json: {message: "Order Destroyed."}
+    else
+      render json: {message: "Unauthorized"}, status: 401
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0}
-  validates :image_url, presence: true
+
 
   def is_discounted?
     price < 10
